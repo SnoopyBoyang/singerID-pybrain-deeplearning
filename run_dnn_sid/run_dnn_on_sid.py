99dae47704f4381f70aa0e88838b1131fd1829ff
zhangxulong <daleloogn@126.com>
2015-08-26 14:52:32 +0800

    0826 debug the classNO. change the singer name to 20 class from 0 to 19

--- run_dnn_sid/run_dnn_on_sid.py
+++ run_dnn_sid/run_dnn_on_sid.py
@@ -12,7 +12,7 @@ train_data = trainset[0]
 train_target = trainset[1]
 
 # set the layers parm,and the max is 8 now
-layers = [10, 20, 10, 20,10,20]
+layers = [10, 20, 10,20]
 # run the dnn ,first autoencoder and then DNNRegressor
 autoencoder = dnn.AutoEncoder(train_data, train_data, train_target, layers, hidden_layer="TanhLayer", final_layer="SoftmaxLayer", compression_epochs=5, bias=True, autoencoding_only=True, dropout_on=True)
 print("1here is okay")#============================================================
