a967ac2ed02fa5710ace9575abc6942021bd4e79
zhangxulong <daleloogn@126.com>
2015-09-01 20:50:34 +0800

    9.1 only the pybrain dnn in this repo delete the theano one and new another repo for the theano dbn

--- README.md
+++
@@ -1,2 +0,0 @@
-# singerID-pybrain-deeplearning
-基于pybrain实现的深度学习网络用于歌手识别
--- use_dbn_on_bigset/__init__.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'Administrator'
-#   1.preper the music data in 'musics' file, and the music format must be '.wav'
-#   2.run the pkl_music_featuer_lable to pkl the dataset
-#   3.run DBN
